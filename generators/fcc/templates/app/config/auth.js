b6e7ede7948575b11a039d3b1c4a7f9c5ff2e98b
Blake Johnston <johnstonbl01@gmail.com>
2015-07-27 14:28:13 -0500

    Remove Twitter auth info.

--- generators/fcc/templates/app/config/auth.js
+++ generators/fcc/templates/app/config/auth.js
@@ -2,8 +2,8 @@
 
 module.exports = {
 	'twitterAuth': {
-		'consumerKey': 'tyfAhHJvq8b4uQ1wH9Tnk8OAD',
-		'consumerSecret': '2piYKYvlqS5vTsPlmL8f5YAOOyuPpVM5UCYepgvV9bdZRd2QqL',
+		'consumerKey': 'yourKeyHere',
+		'consumerSecret': 'yourSecretHere',
 		'callbackURL': 'http://localhost:3000/auth/twitter/callback'
 	}
 };
